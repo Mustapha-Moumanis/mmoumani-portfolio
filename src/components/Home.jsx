3ed2c27c4cfdcf224156c79c6c6d73bf9bb88b59
Mustapha Moumanis <mmoumani@e1r7p3.1337.ma>
2025-04-06 12:29:38 +0100

    update header of index file content for up ranking

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import { useTypewriter } from 'react-simple-typewriter'
 import { motion } from "motion/react"
 import resume from '../assets/pdf/Mustapha-Moumanis-resume.pdf'
 import download_icon from '../assets/images/svg/download-svg.svg'
-import profile_img from '/pic.webp'
+import profile_img from '/Mustapha-Moumanis.webp'
 import SlideUp from '../utlits/animations/slideUp'
 
 const Home = () => {
@@ -14,9 +14,15 @@ const Home = () => {
         <section className="section-one" id="home">
             <div className="row section-one-row">
                 <div className="col-xxl-6 col-xl-6 col-lg-6">
-                    <SlideUp><p className="hi-there">Hi there! <span>👋</span></p></SlideUp>
-                    <SlideUp delay={2}><h1 className="creative">Full-Stack Developer.</h1></SlideUp>
-                    <SlideUp delay={3}><h2 className="visionary">My name is Mustapha Moumanis, and I'm a Full-Stack Developer based in Morocco. I specialize in building dynamic, responsive web applications and I am passionate about creating seamless, innovative digital experiences from both front-end to back-end.</h2></SlideUp>
+                    <SlideUp>
+                        <p className="hi-there">Hi there! <span>👋</span></p>
+                    </SlideUp>
+                    <SlideUp delay={2}>
+                        <h1 className="creative">Mustapha Moumanis</h1>
+                    </SlideUp>
+                    <SlideUp delay={3}>
+                        <h2 className="visionary">I'm a <strong>Full-Stack Developer</strong> based in Morocco. I specialize in building dynamic, responsive web applications and I am passionate about creating seamless, innovative digital experiences from both front-end to back-end.</h2>
+                    </SlideUp>
 
                     <div className="download-btn">
                         <a href={resume} className="aryaBtn" download='resume'>
@@ -34,7 +40,7 @@ const Home = () => {
                         }}
                     >
                         <div className="position-relative zoom_in">
-                            <img className="portfolio-sec-img" src={profile_img} alt="moumanis mustapha"></img>
+                            <img className="portfolio-sec-img" src={profile_img} alt="Mustapha Moumanis — Full-Stack Developer based in Morocco" title="Mustapha Moumanis"></img>
                             
                             <div className="sec-name">
                                 <p className="ityped">{text}</p>
